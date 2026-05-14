7e31a786f6040babe2b803ebeb49a4c96008e52e
Imanol Relloso Hernando <imanol_relloso@euneiz.com>
2026-05-14 17:11:07 +0200

    Error: deteccion de colision

--- Assets/Scripts/Player/PlayerJump.cs
+++ Assets/Scripts/Player/PlayerJump.cs
@@ -10,20 +10,72 @@ public class PlayerJump : MonoBehaviour
     [SerializeField]
     private float jumpForce;
     [SerializeField]
-    private float impulseY;
+    private float wallJumpForceX;
+    [SerializeField]
+    private float wallJumpForceY;
+    [SerializeField]
+    private float slipForce;
 
+    private bool onWall;
     private bool onCollision;
     private Vector3 interpolateWallNormal;
+
+    private Dictionary<GameObject, Collision> collisions;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+        collisions = new Dictionary<GameObject, Collision>();
+    }
+    private void FixedUpdate()
+    {
+        if(onWall)
+            Slip();
+    }
+    private void Slip()
+    {
+        rb.AddForce(-transform.up * slipForce);
     }
     public void OnJump(InputAction.CallbackContext context)
     {
         if (onCollision)
         {
-            rb.AddForce(jumpForce * (interpolateWallNormal.normalized + transform.up * impulseY), ForceMode.Impulse);
+            if (!onWall)
+            {
+                rb.AddForce(jumpForce * transform.up, ForceMode.Impulse);
+            }
+            else
+            {
+                rb.AddForce((interpolateWallNormal.normalized * wallJumpForceX) + (transform.up * wallJumpForceY), ForceMode.Impulse);
+            }
         }
+    }
+
+    private void Update()
+    {
+        CalculateCollision();
+    }
+
+    private void CalculateCollision()
+    {
+        if (!onCollision) return;
+
+        interpolateWallNormal = Vector3.zero;
+
+        foreach(KeyValuePair<GameObject, Collision> key in collisions)
+        {
+            Vector3 temp = Vector3.zero;
+            foreach (ContactPoint point in key.Value.contacts)
+            {
+                temp += point.normal;
+            }
+            Debug.Log(temp.normalized);
+            interpolateWallNormal += temp.normalized;
+        }
+
+        float colAngle = Vector3.Angle(interpolateWallNormal.normalized, transform.up);
+        Debug.Log(colAngle);
+
+        onWall = (colAngle > 60.0f && colAngle < 120f) ? true : false;
     }
 
     private void OnCollisionEnter(Collision collision)
@@ -36,17 +88,25 @@ public class PlayerJump : MonoBehaviour
     }
     private void OnCollisionExit(Collision collision)
     {
+        collisions.Remove(collision.gameObject);
         onCollision = false;
     }
 
     private void OnCollision(Collision coll)
     {
-        onCollision = true;
-        interpolateWallNormal = Vector3.zero;
-
-        foreach (ContactPoint contactPoint in coll.contacts)
+        if (collisions.ContainsKey(coll.gameObject))
         {
-            interpolateWallNormal += contactPoint.normal;
+            onCollision = true;
+            return;
         }
+
+        collisions[coll.gameObject] = coll;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawRay(transform.position, interpolateWallNormal);
     }
 }
--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,15 +6,19 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody rb;
 
     [SerializeField] 
-    private float speed;
+    private float normalSpeed;
     [SerializeField] 
     private float sprintSpeed;
+    [SerializeField] 
+    private float damping = 2.0f;
 
     private Vector2 moveDir;
     private bool sprint;
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
+
+        rb.linearDamping = damping;
     }
     public void OnMove(InputAction.CallbackContext context)
     {
@@ -25,9 +29,25 @@ public class PlayerMovement : MonoBehaviour
         sprint = context.ReadValueAsButton();
     }
 
+    private void Update()
+    {
+        SpeedControl();
+    }
     private void FixedUpdate()
     {
         Vector3 move = transform.forward * moveDir.y + transform.right * moveDir.x;
-        rb.AddForce(move * (sprint ? sprintSpeed : speed));
+        rb.AddForce(move * (sprint ? sprintSpeed : normalSpeed));
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        float speed = sprint ? sprintSpeed : normalSpeed;
+
+        if (flatVel.magnitude > speed)
+        {
+            Vector3 limitedVel = flatVel.normalized * speed;
+            rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+        }
     }
 }
